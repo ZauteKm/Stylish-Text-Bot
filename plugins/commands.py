11cf6540e25d09f43f0ff5262bd5f50f2a133f27
Mr. Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-06-17 16:20:09 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -8,7 +8,7 @@ from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
 @Client.on_message(filters.command('start'))
 async def start(c, m):
     owner = await c.get_users(int(Config.OWNER_ID))
-    owner_username = owner.username if owner.username else 'zautebot'
+    owner_username = owner.username if owner.username else 'ZauteKm'
 
     # start text
     text = f"""Hey! {m.from_user.mention(style='md')},
