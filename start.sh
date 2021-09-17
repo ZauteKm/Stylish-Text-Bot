edadb8a3ee45b124ef4ec0bb293aa7a962c5026b
Zaute <77600757+ZauteKm@users.noreply.github.com>
2021-09-17 14:50:57 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 echo "Cloning Repo...."
 git clone https://github.com/ZauteKm/Stylish-Text-Bot.git /Stylish-Text-Bot
 cd /Stylish-Text-Bot
-pip3 install -U -r requirements.txt
+pip3 install -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
